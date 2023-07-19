1f5ffb480c2bfe2cda1e643bc21fbe590a1724ac
rushiprajapati1704 <114585583+rushiprajapati1704@users.noreply.github.com>
2023-07-19 16:16:53 +0530

    Update script.js

--- insertion/script.js
+++ insertion/script.js
@@ -3,12 +3,12 @@ function init(){
     var n=20;
      n = document.getElementById('n').value;
     var width = window.innerWidth;
+    if(width<=800 && n>37){
+        alert("Array Size 37 < "+n);
+    }
     if(n>60){
         alert("Array Size is 60 < "+n);
         return;
-    }
-    if(width>=800 && width<=1024){
-        alert("tab");
     }
     array.length=n;
     cmp=0,swp=0;
